a331eb8dd2ffdb8eeffbd2d45a0afe8e19d18406
Dr.Maggus <markus.wolfsburg@gmail.com>
2015-04-19 02:19:10 +0200

    SpawnPoints for Coco

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+	public GameObject enemy;
+	public float spawnTime = 3f;
+	public int maxEnemies;
+	public Transform[] spawnPoints;
+	private int currentEnemies;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+
+	void Spawn () {
+		if (maxEnemies > currentEnemies) {
+			int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+			Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+			Debug.Log("Coco Nr. "+this.currentEnemies+" spawned somewhere!");
+			this.currentEnemies++;
+		}
+	}
+}
